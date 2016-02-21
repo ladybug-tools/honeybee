3c8ab74ab056b2443de03729d7becd80ddbedf56
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-02-20 22:16:40 -0500

    Added notes to test file for Sarith

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
-from honeybee.radiance import *
+"""Test file."""
 
 """
-from honeybee import *
 
-HBSurface(geometry)
-HBSurface.RadMaterial()
+# each surface has radiance properties
+# which include radiance materials, etc.
+hbSrf = HBSurface()
 
-analysisRecepie = GridBasedAnalysisRecepie(sky, testPts)
-project = GridBasedAnalysis(HBSurfaces, analysisRecepi)
-project.write(workingDir)
-project.run(nCPUs = 4, runInBackGround = True, wait = True)
-projct.results
+# use honeybee.radiance.sky to generate the sky
+# test points are implemented in geometry library
+ar = GridBasedAnalysisRecepie(sky, testPts)
+
+# create an analysis - I still need to write the library
+analysis = Analysis.GridBased(hbSrf, ar)
+# write all the files to the folder
+analysis.write(workingDir)
+# run the analysis
+analysis.run(nCPUs=4, runInBackGround=True, wait=True)
+
+print analysis.results
 """
