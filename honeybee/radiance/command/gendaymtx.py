5e6a7285e37f1b8f0c3d747c4ac3e8b6b01a1f27
Sarith <sarith@sarith.in>
2016-03-15 19:44:07 -0400

    Started a file for Gendaymtx

---
+++ honeybee/radiance/command/gendaymtx.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from commandBase import RadianceCommand
+import os
+
+
+class Gendaymtx(RadianceCommand):
+    u"""
+    gendaymtx - generate an annual Perez sky matrix from a weather tape.
+
+    Attributes:
+
+    """
+    pass
