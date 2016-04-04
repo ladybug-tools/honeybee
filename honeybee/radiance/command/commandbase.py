5b12f601f878e9ef49fe1fe6abc398ee82986b32
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-04-04 19:21:09 -0400

    updated file names to be lower case
    @sariths this should do it for you. I needed to use:
    
    git config core.ignorecase false

---
+++ honeybee/radiance/command/commandbase.py
@@ -0,0 +1,114 @@
+"""Radiance base command."""
+from ... import config
+
+from abc import ABCMeta, abstractmethod, abstractproperty
+import os
+import subprocess
+
+
+# TODO: Implement piping as on option
+class RadianceCommand(object):
+    """Base class for commands."""
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        """Initialize Radiance command."""
+        self.radbinPath = config.radbinPath
+        self.radlibPath = config.radlibPath
+
+    @property
+    def radbinPath(self):
+        """Get and set path to radiance binaries.
+
+        If you set a new value the value will be changed globally.
+        """
+        return config.radbinPath
+
+    @radbinPath.setter
+    def radbinPath(self, path):
+        self.__checkExecutable(radbinPath=path, raiseException=True)
+        # change the path in config so user need to set it up once in a single script
+        config.radbinPath = path
+
+    @property
+    def radlibPath(self):
+        """Get and set path to radiance libraries.
+
+        If you set a new value the value will be changed globally.
+        """
+        return config.radlibPath
+
+    @radlibPath.setter
+    def radlibPath(self, path):
+        self.__checkLibs(radlibPath=path, raiseException=True)
+        # change the path in config so user need to set it up once in a single script
+        config.radlibPath = path
+
+    @abstractmethod
+    def toRadString(self, relativePath=False):
+        """Return full command as a string."""
+        pass
+
+    @abstractproperty
+    def inputFiles(self):
+        """Return list of input files for this command."""
+        pass
+
+    def __checkExecutable(self, radbinPath=None, raiseException=False):
+        """Check if executable file exist."""
+        radbinPath = self.radbinPath if not radbinPath else radbinPath
+
+        __executable = os.path.normpath(
+            os.path.join(str(radbinPath), '{}.exe'.format(self.__class__.__name__))
+        )
+
+        if not (os.path.isfile(__executable) and os.access(__executable, os.X_OK)):
+            __err = "Can't find %s.\n" % __executable + \
+                "Use radbinPath method to set the path to " + \
+                "Radiance binaries before executing the command."
+            if raiseException:
+                raise ValueError(__err)
+            else:
+                print __err
+
+    def __checkLibs(self, radlibPath=None, raiseException=False):
+        """Check if path to libraries is set correctly."""
+        radlibPath = self.radlibPath if not radlibPath else radlibPath
+
+        if not os.path.isdir(radlibPath):
+            __err = "Can't find %s.\n" % radlibPath + \
+                "Use radlibPath method to set the path to " + \
+                "Radiance libraries before executing the command."
+            if raiseException:
+                raise ValueError(__err)
+            else:
+                print __err
+
+    def __checkFiles(self, raiseExceptionBin=True, raiseExceptionLib=True):
+        """Check if the input files exist on the computer."""
+        assert len(self.inputFiles) != 0, \
+            "You need at least one file to create an octree."
+
+        for f in self.inputFiles:
+            assert os.path.exists(str(f)), "Invalid Input File: %s doesn't exist" % f
+
+        self.__checkExecutable(raiseException=True)
+        self.__checkLibs(raiseException=True)
+
+    # TODO: Add Error handling
+    def execute(self):
+        """Execute the command."""
+        # check if the files exist on the computer
+        self.__checkFiles()
+
+        p = subprocess.Popen(self.toRadString(), shell=True,
+                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+        for line in p.stdout.readlines():
+            print line,
+        p.wait()
+
+    def __repr__(self):
+        """Class representation."""
+        return self.toRadString()
---
+++ honeybee/radiance/recipe/recipebase.py
@@ -0,0 +1,197 @@
+"""Radiance Analysis Recipes."""
+from abc import ABCMeta, abstractmethod
+from ...helper import preparedir
+
+import os
+from collections import namedtuple
+
+
+class HBDaylightAnalysisRecipe(object):
+    """Analysis Recipe Base class.
+
+    Attributes:
+        sky: A honeybee sky for the analysis.
+        simulationType: 0: Illuminance(lux), 1: Radiation(kWh), 2: Luminance(Candela)
+            (Default: 0)
+        radParameters: Radiance parameters for this analysis.
+            (Default: RadianceParameters.LowQuality)
+        hbObjects: An optional list of Honeybee surfaces or zones (Default: None).
+        subFolder: Sub-folder for this analysis recipe. (e.g. "gridbased")
+    """
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, sky, simulationType=None, hbObjects=None, subFolder=""):
+        """Create Analysis recipe."""
+        self.sky = sky
+        """A honeybee sky for the analysis."""
+
+        self.simulationType = simulationType
+        """Simulation type: 0: Illuminance(lux), 1: Radiation (kWh), 2: Luminance (Candela)
+            (Default: 0)
+        """
+
+        self.simulationType = simulationType
+
+        self.hbObjects = hbObjects
+        """An optional list of Honeybee surfaces or zones. (Default: None)"""
+
+        self.subFolder = subFolder
+        """Sub-folder for this analysis recipe. (e.g. "gridbased", "imagebased")"""
+
+        self.isCalculated = False
+        self.isChanged = True
+
+    @property
+    def isAnalysisRecipe(self):
+        """Return true to indicate it is an analysis recipe."""
+        return True
+
+    @property
+    def simulationType(self):
+        """Get/set simulation Type.
+
+        0: Illuminance(lux), 1: Radiation (kWh), 2: Luminance (Candela) (Default: 0)
+        """
+        return self.__simType
+
+    @simulationType.setter
+    def simulationType(self, value):
+        try:
+            value = int(value)
+        except:
+            value = 0
+
+        assert 0 <= value <= 2, \
+            "Simulation type should be between 0-2. Current value: {}".format(value)
+
+        # If this is a radiation analysis make sure the sky is climate-based
+        if value == 1:
+            assert self.sky.isClimateBased, \
+                "The sky for radition analysis should be climate-based."
+
+        self.__simType = value
+
+    @property
+    def hbObjects(self):
+        """Get and set Honeybee objects for this recipe."""
+        return self.__hbObjs
+
+    @hbObjects.setter
+    def hbObjects(self, hbObjects):
+        if not hbObjects:
+            self.__hbObjs = []
+        else:
+            self.__hbObjs = [hbo for hbo in hbObjects if hasattr(hbo, 'isHBObject')]
+            _dif = len(hbObjects) - len(self.__hbObjs)
+            if _dif:
+                print "%d non Honeybee objects are found and removed." % _dif
+
+    @property
+    def sky(self):
+        """Get and set sky definition."""
+        return self.__sky
+
+    @sky.setter
+    def sky(self, newSky):
+        assert hasattr(newSky, "isRadianceSky"), "%s is not a valid Honeybee sky." % type(newSky)
+        self.__sky = newSky
+
+    @property
+    def subFolder(self):
+        """Sub-folder for Grid-based analysis."""
+        return self.__folder
+
+    @subFolder.setter
+    def subFolder(self, value):
+        """Sub-folder for Grid-based analysis."""
+        self.__folder = str(value)
+
+    def hbObjectsToRadString(self):
+        """Return geometries and materials as a tuple of multiline string.
+
+        Returns:
+            A namedTuple of multiline data. Keys are: materials geometries
+
+        Usage:
+            s = self.hbObjectsToRadString()
+            geoString = s.geometries
+            matString = s.materials
+            or
+            s = self.hbObjectsToRadString()
+            matString, geoString = s
+        """
+        _radDefinition = namedtuple("RadString", "materials geometries")
+        _matStr = ""
+        _geoStr = ""
+
+        if len(self.hbObjects) > 0:
+            _materials = []
+            _geos = []
+            for hbo in self.hbObjects:
+                try:
+                    # hbzone or HBSurface with child surface returns a list of materials
+                    _materials.extend(hbo.radianceMaterial)
+                except:
+                    # hb surface with no child surface
+                    _materials.append(hbo.radianceMaterial)
+
+                _geos.append(hbo.toRadString())
+
+            # remove duplicated materials
+            _materials = set([mat.toRadString() for mat in _materials])
+            _matStr = "\n".join(_materials)
+
+            # joing geometries
+            _geoStr = "\n".join(_geos)
+
+            print "Number of total materials: %d" % len(_materials)
+            print "Number of total surfaces: %d" % len(_geos)
+        else:
+            print "Warning: Found no Honeybee objects."
+
+        return _radDefinition(_matStr, _geoStr)
+
+    @abstractmethod
+    def toRadString(self):
+        """Radiance representation of the recipe."""
+        pass
+
+    @abstractmethod
+    def writeToFile(self, targetFolder):
+        """Write files for the analysis recipe to file.
+
+        Args:
+            filePath: Full path for a valid file path (e.g. c:/ladybug/testPts.pts)
+
+        Returns:
+            True in case of success. False in case of failure.
+        """
+        pass
+
+    # TODO: Check for white space in names. I need to test some cases to make
+    # sure what will and what will not fail for Radiance before implementing this.
+    @staticmethod
+    def write(filePath, data, mkdir=False):
+        """Write a string of data to file.
+
+        Args:
+            filePath: Full path for a valid file path (e.g. c:/ladybug/testPts.pts)
+            data: Any data as string
+            mkdir: Set to True to create the directory if doesn't exist (Default: False)
+        """
+        __dir, __name = os.path.split(filePath)
+
+        if not os.path.isdir(__dir):
+            if mkdir:
+                preparedir(__dir)
+            else:
+                raise ValueError("Target dir doesn't exist: %s" % __dir)
+
+        with open(filePath, "w") as outf:
+            try:
+                outf.write(str(data))
+                return filePath
+            except Exception as e:
+                print "Failed to write points to file:\n%s" % e
+                return False
