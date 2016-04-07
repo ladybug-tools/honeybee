15b8d11bd4100f003473e43ca48d9f5a545e5132
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-04-06 22:17:28 -0400

    Added readme to testrun folder
    Just ran tests and figured that the folder has been removed for some
    reason. Adding the folder back with a note.

--- test.py
+++ test.py
@@ -1,35 +1,34 @@
-from honeybee.radiance.sky import *
-print sorted(locals().keys())
-# from honeybee.radiance.parameters.oconv import OconvParameters
-# from honeybee.radiance.command.oconv import Oconv
+
+from honeybee.radiance.parameters.oconv import OconvParameters
+from honeybee.radiance.command.oconv import Oconv
+
+# sky = radSky(1000)
+# rp = HBGridBasedAnalysisRecipe(sky, pointGroups=[0, 0, 0])
 #
-# # sky = radSky(1000)
-# # rp = HBGridBasedAnalysisRecipe(sky, pointGroups=[0, 0, 0])
-# #
-# # print rp.radianceParameters
-# # rp.writeToFile("c:\\ladybug", "test3")
-# # rp.run(debug=False)
-# # print
-# # print rp.results(flattenResults=True)
+# print rp.radianceParameters
+# rp.writeToFile("c:\\ladybug", "test3")
+# rp.run(debug=False)
+# print
+# print rp.results(flattenResults=True)
+
+# generate oconv parameters
+rcp = OconvParameters()
+
+# trun off turn off warnings
+rcp.turnOffWarns = True
 #
-# # generate oconv parameters
-# rcp = OconvParameters()
-#
-# # trun off turn off warnings
-# rcp.turnOffWarns = True
-# #
-# # create an oconv command
-# oconv = Oconv(outputName="test3",
-#               sceneFiles=((r"C:\ladybug\test3\gridbased\Uniform_CIE_1000.sky",
-#                            r"C:\ladybug\test3\gridbased\test3.mat",
-#                            r"c:\ladybug\test3\gridbased\test3.rad")),
-#               oconvParameters=rcp
-#               )
-#
-# # print command line to check
-# print oconv.toRadString()
-#
-# # execute the command
-# outputFilePath = oconv.execute()
-#
-# print outputFilePath
+# create an oconv command
+oconv = Oconv(outputName="test3",
+              sceneFiles=((r"C:\ladybug\test3\gridbased\Uniform_CIE_1000.sky",
+                           r"C:\ladybug\test3\gridbased\test3.mat",
+                           r"c:\ladybug\test3\gridbased\test3.rad")),
+              oconvParameters=rcp
+              )
+
+# print command line to check
+print oconv.toRadString()
+
+# execute the command
+outputFilePath = oconv.execute()
+
+print outputFilePath
