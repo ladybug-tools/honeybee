08846f2b7471c27112511c3d6cde22f08e52c418
Sarith <sarith@sarith.in>
2016-05-19 17:46:19 -0400

    Updated test.py 1. test.py will write files inside the temp folder. 2. This ensures that ciritical files inside room won't be tampered with.

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ import os
 os.chdir(r'tests\room')
 
 #Step1: Create annual daylight vectors through epw2wea and gendaymtx.
-weaFile = Epw2wea(epwFile='test.epw')
+weaFile = Epw2wea(epwFile='test.epw',outputWeaFile=r'temp\test.wea')
 weaFile.execute()
 
-genday = Gendaymtx(weaFile='test.wea',outputName='day.smx')
+genday = Gendaymtx(weaFile=r'temp\test.wea',outputName=r'temp\day.smx')
 genday.execute()
 
 #Step2: Generate daylight coefficients using rfluxmtx.
@@ -25,20 +25,19 @@ rfluxPara.ab = 5
 
 rflux = Rfluxmtx()
 rflux.sender = '-'
-skyFile = rflux.defaultSkyGround('rfluxSky.rad')
+skyFile = rflux.defaultSkyGround(r'temp\rfluxSky.rad')
 rflux.receiverFile = skyFile
 rflux.rfluxmtxParameters = rfluxPara
 rflux.radFiles = [r"room.mat",
                   r"room.rad"]
 
 rflux.pointsFile = r"indoor_points.pts"
-rflux.outputMatrix = r"dayCoeff.dc"
-print(rflux.toRadString())
+rflux.outputMatrix = r"temp\dayCoeff.dc"
 rflux.execute()
 
 
-mtx1 = Rmtxop(matrixFiles=(os.path.abspath(r'dayCoeff.dc'),os.path.abspath(r'day.smx')),
-              outputFile=r'illuminance.tmp')
+mtx1 = Rmtxop(matrixFiles=(os.path.abspath(r'temp\dayCoeff.dc'),os.path.abspath(r'temp\day.smx')),
+              outputFile=r'temp\illuminance.tmp')
 mtx1.execute()
 
 
@@ -46,11 +45,11 @@ mtx2Param = RmtxopParameters()
 mtx2Param.outputFormat = 'a'
 mtx2Param.combineValues = (47.4,119.9,11.6)
 mtx2Param.transposeMatrix = True
-mtx2 = Rmtxop(matrixFiles=['illuminance.tmp'],outputFile='illuminance.ill')
+mtx2 = Rmtxop(matrixFiles=[r'temp\illuminance.tmp'],outputFile=r'temp\illuminance.ill')
 mtx2.rmtxopParameters = mtx2Param
 mtx2.execute()
 
-with open('illuminance.ill') as illFile:
+with open(r'temp\illuminance.ill') as illFile:
     for lines in illFile:
         try:
             print(map(float,lines.split()))
