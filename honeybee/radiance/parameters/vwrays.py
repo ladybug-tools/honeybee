67d24b3e93b0cbf8733b30c4c2860219446ed446
Sarith Subramaniam <sarith@sarith.in>
2016-06-27 01:28:47 -0700

    Modest start for vwrays. 1. Added views as per existing rad files.

---
+++ honeybee/radiance/parameters/vwrays.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+from ._advancedparametersbase import AdvancedRadianceParameters
+from ._frozen import frozen
+
+
+@frozen
+class VwraysParameters(AdvancedRadianceParameters):
+    pass
