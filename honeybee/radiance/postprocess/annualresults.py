b93650e0fc67128054b976578ac695c9917927e6
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-11-20 22:36:46 -0500

    Added methods to parse annual daylight results

--- honeybee/radiance/postprocess/annualresults.py
+++ honeybee/radiance/postprocess/annualresults.py
@@ -10,15 +10,13 @@ class LoadAnnualsResults(object):
             file. (Default: True)
     """
 
-    def __init__(self, resultFiles=[], flattenResults=True):
+    def __init__(self, resultFiles=None, flattenResults=True):
         """Init the class."""
-        self.resultFiles = resultFiles
+        self.resultFiles = resultFiles or []
         """A list of result files."""
 
         self.flattenResults = flattenResults
-        """
-        Set to False to get a separate list of results for each file. (Default: True)
-        """
+        """Set to False to get a separate list of results for each file (Default: True)."""
 
         self.__results = []
         self.__isLoaded = False
@@ -68,3 +66,69 @@ class LoadAnnualsResults(object):
                     # print "{} Hours >> {}".format(sum(results), results)
         except Exception as e:
             print "Failed to load the results from {}: {}".format(resultFile, e)
+
+
+def parseline(l, DATASIZE):
+    """Parse results line."""
+    yield (float(l[DATASIZE * hour:DATASIZE * (hour + 1) - 1]) for hour in xrange(8760))
+
+
+def calculateDA(hourlydata, schedule=None, t=300):
+    """Calculate DA for a point based on annual results."""
+    hourlydata = iter(hourlydata)
+    da = 0
+    if not schedule:
+        schedule = [1] * 8760
+    for count, v in enumerate(next(hourlydata)):
+        if schedule[count] and v >= t:
+            da += 1
+
+    yield (da / float(count)) * 100
+
+
+def readAnuualResultsByPointIndex(f, ind):
+    """Read annual results for a single sensor based on index number."""
+    pointCount, HEADERSIZE, LINESIZE, DATASIZE = getSizes(f)
+    with open(f) as inf:
+        inf.seek(LINESIZE * ind + HEADERSIZE)
+        yield parseline(inf.read(LINESIZE), DATASIZE)
+
+
+def readHourlyResults(f, HOY, startPoint=0, endPoint=None):
+    """Get the results for an hour of the year for several sensors."""
+    pointCount, HEADERSIZE, LINESIZE, DATASIZE = getSizes(f)
+
+    with open(f) as inf:
+        for ptCount in xrange(startPoint, endPoint):
+            inf.seek(HEADERSIZE + LINESIZE * ptCount + DATASIZE * HOY)
+            yield inf.read(DATASIZE - 1)
+
+
+def getDaylightAutonomy(f, startPoint=0, endPoint=None):
+    """Get daylight autonomy values for points between start and end."""
+    pointCount, HEADERSIZE, LINESIZE, DATASIZE = getSizes(f)
+    if not endPoint:
+        endPoint = pointCount
+
+    with open(f) as inf:
+        for ptCount in xrange(startPoint, endPoint):
+            inf.seek(LINESIZE * ptCount + HEADERSIZE)
+            yield calculateDA(parseline(inf.read(LINESIZE), DATASIZE))
+
+
+def getSizes(f):
+    """Get size of header, line , and each result value."""
+    with open(f) as inf:
+        for l in range(3):
+            line = inf.readline()
+        NUMBEROFPOINTS = int(line.split('=')[-1])
+        for l in range(4):
+            inf.readline()
+        HEADERSIZE = inf.tell()
+        inf.readline()
+        LINESIZE = inf.tell() - HEADERSIZE
+        return NUMBEROFPOINTS, HEADERSIZE, LINESIZE, int(LINESIZE / 8760)
+
+if __name__ == '__main__':
+    f = r"C:\ladybug\untitled\annualdaylight\illuminance.ill"
+    ad = tuple(next(v) for count, v in enumerate(getDaylightAutonomy(f)))
