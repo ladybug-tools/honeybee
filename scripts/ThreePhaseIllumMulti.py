6b084a1c29ad92ba6402d7151583817a4dee8cb9
sariths <sarith@sarith.in>
2016-11-25 17:16:16 -0500

    Three Phase MultiWinGroups 1. Now does images too

--- scripts/ThreePhaseIllumMulti.py
+++ scripts/ThreePhaseIllumMulti.py
@@ -16,6 +16,16 @@ if 'honeybee' not in sys.modules:
     sys.path.insert(
         0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from sarith.results.rad2Daysim import annual2daysim,single2daysim
+from sarith.visuals.gridPlots import gridPlot
+from sarith.results.ill import Illarr
+from sarith.results.dayIll import Dayill
+from sarith.results.pts import RoomGrid
+from sarith.radiance.radbin import  *
+from sarith.calcs.weather.wea import Wea
+from sarith.utilities.files.paths import UserPath
+import os
+
 from honeybee.radiance.parameters.rfluxmtx import RfluxmtxParameters
 from honeybee.radiance.command.rfluxmtx import Rfluxmtx
 from honeybee.radiance.command.epw2wea import Epw2wea
@@ -24,16 +34,19 @@ from honeybee.radiance.command.dctimestep import Dctimestep
 from honeybee.radiance.command.genskyvec import Genskyvec
 from honeybee.radiance.command.gensky import Gensky
 from honeybee.radiance.command.rmtxop import Rmtxop, RmtxopParameters
+from honeybee.radiance.command.pcomb import PcombImage,PcombParameters,Pcomb
+from honeybee.radiance.command.vwrays import Vwrays,VwraysParameters
 
 
 
-
-def run3phaseMulti(pointsFile,outputIllName,windowConfig=(),epwFile=None,skyFile=None,
+def run3phaseMulti(pointsFile=None,outputIllName=None,viewFile=None,outputImageName=None,
+                   windowConfig=(),epwFile=None,skyFile=None,
                    geometryFile=None,materialFile=None,
                    reuseVmtx=True,reuseSkyVector=True,reuseDmtx=True,
                    skyDescr=1,klemsForVmtx='kf',
                    vmtxParam=None,dmtxParam=None,TransposeAnnualResults=True,
-                   skyVectorParam=None):
+                   skyVectorParam=None,numProcess=1,samplingForImg=9,xResolution=800,
+                   yResolution=800,jitterForImage=0.7,reuseExistingImageRays=True):
     """
     Notes:
         1. It is assumed that the surfaces used for the vmtx are inward facing .i.e they
@@ -80,10 +93,21 @@ def run3phaseMulti(pointsFile,outputIllName,windowConfig=(),epwFile=None,skyFile
             will be assigned.
         TransposeAnnualResults: A boolean value, which if set to True, will transpose
          the results of an annual calculation as per the 8760 lines format.
+        numProcess: Number of processors to be used for calculations.
+        viewFile: If specified instead of a points file, an image-based three phase method
+            calculation will be performed.
+        outputImageName: The name of the output image file if a an image based calculation
+            is to be performed.
+
 
     Returns:
 
+
+
     """
+
+    assert viewFile or pointsFile,\
+        'Either a viewFile or a pointsFile must be specified for the calculation.'
 
     def createWinGroupReceivers(surfaceFile, surfaceNormal, klemsBasis):
         #remove the + or - sign if specified.
@@ -133,15 +157,86 @@ def run3phaseMulti(pointsFile,outputIllName,windowConfig=(),epwFile=None,skyFile
 
 
         rflux = Rfluxmtx()
+        rflux.numProcessors=numProcess
         rflux.receiverFile = receiverFile
         rflux.rfluxmtxParameters = vmtxParam
         rflux.pointsFile = pointsFile
         rflux.sender = '-'
         rflux.outputMatrix = vmtxFile
         rflux.radFiles = [materialFile,geometryFile]
-        print (rflux.toRadString())
         rflux.execute()
         return vmtxFile
+
+    def createRaysFromViewFile(viewFile,sampling,xRes,yRes,jitterVal,reuseExistingRays):
+        viewFileName=os.path.splitext(os.path.split(viewFile)[-1])[0]
+        vwrParaDim = VwraysParameters()
+        vwrParaDim.calcImageDim = True
+        vwrParaDim.xResolution = xRes
+        vwrParaDim.yResolution = yRes
+
+        vwrDim = Vwrays()
+        vwrDim.vwraysParameters = vwrParaDim
+        vwrDim.viewFile = viewFile
+        vwrDim.outputFile = r'temp/%s.tmprays'%(viewFileName)
+        if reuseExistingRays and not os.path.exists(r'temp/%s.tmprays'%(viewFileName)):
+            vwrDim.execute()
+
+        vwrParaSamp = VwraysParameters()
+        vwrParaSamp.xResolution = xRes
+        vwrParaSamp.yResolution = yRes
+        vwrParaSamp.samplingRaysCount = sampling
+        vwrParaSamp.jitter = jitterVal
+
+
+        vwrSamp  = Vwrays()
+        vwrSamp.vwraysParameters = vwrParaSamp
+        vwrSamp.viewFile = viewFile
+        vwrSamp.outputFile = r'temp/%s.rays'%(viewFileName)
+        vwrSamp.outputDataFormat = 'f'
+        if reuseExistingRays and not os.path.exists(r'temp/%s.rays'%(viewFileName)):
+            vwrSamp.execute()
+
+        return {'rays':vwrSamp.outputFile,'rayInfo':vwrDim.outputFile}
+
+
+    def calcVmtxImg(surfaceFile,receiverFile, reuse, vmtxParam, imageRaysDict,materialFile,
+                 geometryFile,sampling):
+        surfaceName = os.path.splitext(surfaceFile)[0]
+        outputFileNameFormat = r'temp/%s'%surfaceName+'%03d.hdr'
+
+        if os.path.exists(r'temp/%s000.hdr'%surfaceName) and reuse:
+            return outputFileNameFormat
+
+        if vmtxParam:
+            assert isinstance(vmtxParam,RfluxmtxParameters),\
+                'The input for vmtxParam must be an instance of RfluxmtxParamters. ' \
+                'The current input is an instance of %s'%(type(vmtxParam))
+        else:
+            vmtxParam=RfluxmtxParameters()
+            vmtxParam.ambientAccuracy = 0.1
+            vmtxParam.ambientBounces = 4
+            vmtxParam.ambientDivisions = 1000
+            vmtxParam.limitWeight = 1E-5
+
+
+
+        rflux = Rfluxmtx()
+        rflux.numProcessors=numProcess
+        rflux.samplingRaysCount = sampling
+        rflux.receiverFile = receiverFile
+        rflux.rfluxmtxParameters = vmtxParam
+        rflux.sender = '-'
+        rflux.radFiles = [materialFile,geometryFile]
+        rflux.outputDataFormat = 'fc'
+        rflux.verbose = True
+        rflux.rfluxmtxParameters = vmtxParam
+        rflux.viewInfoFile = imageRaysDict['rayInfo']
+        rflux.viewRaysFile = imageRaysDict['rays']
+        rflux.outputFilenameFormat = outputFileNameFormat
+
+        rflux.execute()
+
+        return outputFileNameFormat
 
     def calcDmtx(surfaceFile,senderFile,reuse,skyDescr,dmtxParam,
                  materialFile,geometryFile):
@@ -162,6 +257,7 @@ def run3phaseMulti(pointsFile,outputIllName,windowConfig=(),epwFile=None,skyFile
             dmtxParam.limitWeight = 1E-5
 
         rflux2 = Rfluxmtx()
+        rflux2.numProcessors=numProcess
         rflux2.samplingRaysCount = 1000
         rflux2.sender = senderFile
         skyFile = rflux2.defaultSkyGround(r'temp/rfluxSky.rad', skyType="r%s"%skyDescr)
@@ -227,15 +323,28 @@ def run3phaseMulti(pointsFile,outputIllName,windowConfig=(),epwFile=None,skyFile
     skyVector = calcSkyVector(skyDescr=skyDescr,epwFile=epwFile,skyFile=skyFile,
                               reuse=reuseSkyVector, skyVectorParam=skyVectorParam)
 
+
+    if viewFile:
+        imageRaysDict = createRaysFromViewFile(viewFile=viewFile,sampling=samplingForImg,
+                                               xRes=xResolution,yRes=yResolution,
+                                               jitterVal=jitterForImage,
+                                               reuseExistingRays=reuseExistingImageRays)
+    else:
+        imageRaysDict=None
+
     #create V and D matrices corresponding to each window Group.
 
     matrixFilesForResult = []
     resultFiles = 0
     for idx,recFile in enumerate(receiverFiles):
-
-        vmtxFile = calcVmtx(windowConfig[idx]['windowSurface'],recFile,reuse=reuseVmtx,
-                 vmtxParam=vmtxParam,pointsFile=pointsFile,materialFile=materialFile,
-                 geometryFile=geometryFile)
+        if pointsFile:
+            vmtxFile = calcVmtx(windowConfig[idx]['windowSurface'],recFile,reuse=reuseVmtx,
+                     vmtxParam=vmtxParam,pointsFile=pointsFile,materialFile=materialFile,
+                     geometryFile=geometryFile)
+        elif viewFile:
+            vmtxFile = calcVmtxImg(windowConfig[idx]['windowSurface'],recFile,reuse=reuseVmtx,
+                     vmtxParam=vmtxParam,materialFile=materialFile,
+                     geometryFile=geometryFile,sampling=samplingForImg,imageRaysDict=imageRaysDict)
 
         dmtxFile = calcDmtx(windowConfig[idx]['windowSurface'],senderFile=recFile,
                             skyDescr=skyDescr, dmtxParam=dmtxParam,
@@ -254,29 +363,39 @@ def run3phaseMulti(pointsFile,outputIllName,windowConfig=(),epwFile=None,skyFile
             dct.tmatrixFile = windowConfig[idx]['tMatrix']
             dct.outputFileName = dctResult
             dct.execute()
-            matrixFilesForResult.append(dctResult)
+            if viewFile:
+                dctResultImg = PcombImage()
+                dctResultImg.inputImageFile =dctResult
+            matrixFilesForResult.append(dctResultImg)
 
             resultFiles +=1
     assert resultFiles,'None of the Window Groups were chosen for calculating the results !'
 
-    rmtxAdd = Rmtxop()
-    rmtxAdd.matrixFiles = matrixFilesForResult
-    rmtxAdd.outputFile = os.path.splitext(outputIllName)[0]+'.tmp'
-    rmtxAdd.execute()
+    if pointsFile:
+        rmtxAdd = Rmtxop()
+        rmtxAdd.matrixFiles = matrixFilesForResult
+        rmtxAdd.outputFile = os.path.splitext(outputIllName)[0]+'.tmp'
+        rmtxAdd.execute()
 
-    rmtResParam = RmtxopParameters()
-    rmtResParam.combineValues = (47.4, 119.9, 11.6)
-    rmtResParam.outputFormat = 'a'
-    rmtResParam.transposeMatrix = TransposeAnnualResults
+        rmtResParam = RmtxopParameters()
+        rmtResParam.combineValues = (47.4, 119.9, 11.6)
+        rmtResParam.outputFormat = 'a'
+        rmtResParam.transposeMatrix = TransposeAnnualResults
 
-    rmtxRes = Rmtxop()
-    rmtxRes.matrixFiles = [rmtxAdd.outputFile]
-    rmtxRes.outputFile = outputIllName
-    rmtxRes.rmtxopParameters = rmtResParam
-    rmtxRes.execute()
+        rmtxRes = Rmtxop()
+        rmtxRes.matrixFiles = [rmtxAdd.outputFile]
+        rmtxRes.outputFile = outputIllName
+        rmtxRes.rmtxopParameters = rmtResParam
+        rmtxRes.execute()
 
-    return outputIllName
+        return outputIllName
+    else:
+        pcmb = Pcomb()
+        pcmb.imageList = matrixFilesForResult
+        pcmb.outputImageFile = outputImageName
+        pcmb.execute()
 
+        return outputImageName
 
 if __name__ == "__main__":
     os.chdir(r'../tests')
@@ -290,32 +409,41 @@ if __name__ == "__main__":
 
     if True:
         gensk = Gensky()
-        gensk.monthDayHour = (11, 11, 11)
+        gensk.monthDayHour = (6, 31,9)
         gensk.outputFile = 'temp/sky.rad'
         gensk.execute()
         skyFile='temp/sky.rad'
     else:
         skyFile = None
 
+    indexNum=2
     #creating WindowConfigurations.
     win1 = {'windowSurface':'glazingEast.rad',
             'surfaceNormal':'+X',
-            'tMatrix':xmlFiles[0],
+            'tMatrix':xmlFiles[indexNum],
             'includeInCalc':True}
 
     win2 = {'windowSurface':'glazingSkylight.rad',
             'surfaceNormal':'+Z',
-           'tMatrix':xmlFiles[0],
+           'tMatrix':xmlFiles[2],
             'includeInCalc':True}
 
     win3 = {'windowSurface':'glazingSouth.rad',
             'surfaceNormal':'+Y',
-            'tMatrix':xmlFiles[0],
+            'tMatrix':xmlFiles[indexNum],
             'includeInCalc':True}
+
+    # run3phaseMulti(windowConfig=(win1,win2,win3), epwFile=None, skyFile=skyFile,
+    #                geometryFile='geometry.rad', materialFile='materials.rad',
+    #                reuseVmtx=True, reuseSkyVector=True, reuseDmtx=True,
+    #                pointsFile='points.txt', skyDescr=1, klemsForVmtx='kf',
+    #                vmtxParam=None, dmtxParam=None, TransposeAnnualResults=True,
+    #                skyVectorParam=None,outputIllName=r'temp/3ph1.ill',numProcess=8)
 
     run3phaseMulti(windowConfig=(win1,win2,win3), epwFile=None, skyFile=skyFile,
                    geometryFile='geometry.rad', materialFile='materials.rad',
-                   reuseVmtx=True, reuseSkyVector=True, reuseDmtx=True,
-                   pointsFile='points.txt', skyDescr=1, klemsForVmtx='kf',
+                   reuseVmtx=True, reuseSkyVector=False, reuseDmtx=True,
+                   viewFile=r'views/south.vf', skyDescr=1, klemsForVmtx='kf',
                    vmtxParam=None, dmtxParam=None, TransposeAnnualResults=True,
-                   skyVectorParam=None,outputIllName=r'temp/3ph1.ill')
+                   skyVectorParam=None,outputImageName='res.hdr',numProcess=8)
+
