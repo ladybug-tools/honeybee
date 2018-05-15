d74cbc270c395dbd2d7ea385c6bf0bd8923a9d06
LAPTOP-MB0HU4QI\Mostapha <sadeghipour@gmail.com>
2018-05-14 20:32:10 -0400

    Add glow material to properties

--- honeybee/radiance/properties.py
+++ honeybee/radiance/properties.py
@@ -1,6 +1,7 @@
 """Radiance Properties for HBSurfaces."""
 import copy
 from .material.plastic import BlackMaterial
+from .material.glow import WhiteGlow
 
 
 class RadianceProperties(object):
@@ -15,7 +16,7 @@ class RadianceProperties(object):
             material.
     """
 
-    __slots__ = ('_material', '_black_material')
+    __slots__ = ('_material', '_black_material', '_glow_material')
 
     def __init__(self, material=None, black_material=None):
         """Create radiance properties for surface."""
@@ -46,12 +47,18 @@ class RadianceProperties(object):
 
     @property
     def black_material(self):
-        """Return Radiance Material."""
+        """Radiance black material."""
         return self._black_material
 
     @black_material.setter
-    def black_material(self, material):
-        """Set Radiance black_materialblack material."""
+    def black_material(self, material, rename_black_material=False):
+        """Set Radiance black_materialblack material.
+
+        Args:
+            material: A radiance material. Default material is BlackMaterial.
+            rename_black_material: Rename default black material to the same name as
+            radiance material (Default: False).
+        """
         if material:
             # chek if radiance material is radiance material
             assert hasattr(material, 'isRadianceMaterial'), \
@@ -60,6 +67,33 @@ class RadianceProperties(object):
             self._black_material = material
         else:
             self._black_material = BlackMaterial()
+            if self.material and rename_black_material:
+                self._black_material.name = self.radiance.name
+
+    @property
+    def glow_material(self):
+        """Radiance glow material."""
+        return self._glow_material
+
+    @glow_material.setter
+    def glow_material(self, material, rename_glow_material=False):
+        """Set Radiance glow_materialblack material.
+
+        Args:
+            material: A radiance material. Default material is GlowMaterial.
+            rename_glow_material: Rename default glow material to the same name as
+            radiance material (Default: False).
+        """
+        if material:
+            # chek if radiance material is radiance material
+            assert hasattr(material, 'isRadianceMaterial'), \
+                TypeError('Expected RadianceMaterial not {}'.format(type(material)))
+            # set new material
+            self._glow_material = material
+        else:
+            self._glow_material = WhiteGlow()
+            if self.material and rename_glow_material:
+                self._glow_material.name = self.radiance.name
 
     def duplicate(self):
         """Duplicate RadianceProperties."""
