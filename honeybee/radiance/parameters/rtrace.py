c60750ecc2b6ed606bee447c5f8854c6b09c9a9b
Sarith Subramaniam <sarith@sarith.in>
2018-05-22 12:16:47 +0200

    Rtrace paarm for Pmap

--- honeybee/radiance/parameters/rtrace.py
+++ honeybee/radiance/parameters/rtrace.py
@@ -280,6 +280,30 @@ class RtraceParameters(AdvancedRadianceParameters):
         character is missing, the input format is used.
         """
 
+        # Photon-mapping goodies start here!
+        self.add_radiance_value('ac', descriptive_name='photon cache page size',
+                                attribute_name='photon_cache_pagesize')
+        self.photon_cache_pagesize = None
+        """Place holder for comments."""
+
+        # Photon-mapping goodies start here!
+        self.add_radiance_value('am', descriptive_name='maximum search radius',
+                                attribute_name='max_search_radius')
+        self.max_search_radius = None
+        """Place holder for comments."""
+
+        # Photon-mapping goodies start here!
+        self.add_radiance_value('ap', descriptive_name='photon map file and bandwidths',
+                                attribute_name='photon_map_file_bandwidth')
+        self.photon_map_file_bandwidth = None
+        """Place holder for comments."""
+
+        # Photon-mapping goodies start here!
+        self.add_radiance_value('aC', descriptive_name='photon cache size',
+                                attribute_name='photon_cache_size')
+        self.photon_cache_size = None
+        """Place holder for comments."""
+
     @classmethod
     def from_json(cls, rec_json):
         """Create radiance parameters from json.
