baf79c81c2961a4ba51513025746c74d0cb0f4a8
LAPTOP-MB0HU4QI\Mostapha <sadeghipour@gmail.com>
2018-06-07 09:18:24 -0400

    Added direct sun calculation to reflection study
    The recipe is now similar to other recipes and calculates direct and total separately. This will allow separating the results for direct and reflected only.

--- honeybee/radiance/recipe/directreflection/gridbased.py
+++ honeybee/radiance/recipe/directreflection/gridbased.py
@@ -1,5 +1,19 @@
 from ..solaraccess.gridbased import SolarAccessGridBased
 from ...material.mirror import Mirror
+from ..recipeutil import write_rad_files, write_extra_files
+from ..recipedcutil import rgb_matrix_file_to_ill
+
+from ...command.oconv import Oconv
+from ...command.rcontrib import Rcontrib
+from ...analysisgrid import AnalysisGrid
+from ...sky.analemma import Analemma
+from ....futil import write_to_file
+from ....hbsurface import HBSurface
+
+from ladybug.sunpath import Sunpath
+from ladybug.location import Location
+
+import os
 
 
 class DirectReflectionGridBased(SolarAccessGridBased):
@@ -37,8 +51,37 @@ class DirectReflectionGridBased(SolarAccessGridBased):
                                       timestep, hb_objects, sub_folder)
 
         # update radiance paramters
-        # set -ar to 1 to capture first reflection from mirror like surfaces.
+        # set -dr to 1 to capture first reflection from mirror like surfaces.
         self._radiance_parameters.direct_sec_relays = 1
+
+    @classmethod
+    def from_json(cls, rec_json):
+        """Create the solar access recipe from json.
+            {
+              "id": "solar_access",
+              "type": "gridbased",
+              "location": null, // a honeybee location - see below
+              "hoys": [], // list of hours of the year
+              "surfaces": [], // list of honeybee surfaces
+              "analysis_grids": [] // list of analysis grids
+              "sun_vectors": [] // list of sun vectors if location is not provided
+            }
+        """
+        raise NotImplementedError()
+        hoys = rec_json["hoys"]
+        if 'sun_vectors' not in rec_json or not rec_json['sun_vectors']:
+            # create sun vectors from location inputs
+            loc = Location.from_json(rec_json['location'])
+            sp = Sunpath.from_location(loc)
+            suns = (sp.calculate_sun_from_hoy(hoy) for hoy in hoys)
+            sun_vectors = tuple(s.sun_vector for s in suns if s.is_during_day)
+        else:
+            sun_vectors = rec_json['sun_vectors']
+
+        analysis_grids = \
+            tuple(AnalysisGrid.from_json(ag) for ag in rec_json["analysis_grids"])
+        hb_objects = tuple(HBSurface.from_json(srf) for srf in rec_json["surfaces"])
+        return cls(sun_vectors, hoys, analysis_grids, 1, hb_objects)
 
     @classmethod
     def from_suns(cls, suns, point_groups, vector_groups=[], timestep=1,
@@ -117,3 +160,157 @@ class DirectReflectionGridBased(SolarAccessGridBased):
             surface.radiance_properties.black_material = reflective_material
 
         return surfaces
+
+    def write(self, target_folder, project_name='untitled', header=True):
+        """Write analysis files to target folder.
+
+        Files for sunlight hours analysis are:
+            test points <project_name.pts>: List of analysis points.
+            suns file <*.sun>: list of sun sources .
+            suns material file <*_suns.mat>: Radiance materials for sun sources.
+            suns geometry file <*_suns.rad>: Radiance geometries for sun sources.
+            material file <*.mat>: Radiance materials. Will be empty if hb_objects is
+                None.
+            geometry file <*.rad>: Radiance geometries. Will be empty if hb_objects is
+                None.
+            batch file <*.bat>: An executable batch file which has the list of commands.
+                oconv [material file] [geometry file] [sun materials file] [sun
+                    geometries file] > [octree file]
+                rcontrib -ab 0 -ad 10000 -I -M [sunlist.txt] -dc 1 [octree file]< [pts
+                    file] > [rcontrib results file]
+
+        Args:
+            target_folder: Path to parent folder. Files will be created under
+                target_folder/gridbased. use self.sub_folder to change subfolder name.
+            project_name: Name of this project as a string.
+
+        Returns:
+            True in case of success.
+        """
+        # 0.prepare target folder
+        # create main folder target_folder/project_name
+        project_folder = \
+            super(SolarAccessGridBased, self).write_content(target_folder, project_name)
+
+        # write geometry and material files
+        opqfiles, glzfiles, wgsfiles = write_rad_files(
+            project_folder + '/scene', project_name, self.opaque_rad_file,
+            self.glazing_rad_file, self.window_groups_rad_files
+        )
+        # additional radiance files added to the recipe as scene
+        extrafiles = write_extra_files(self.scene, project_folder + '/scene', True)
+
+        # 1.write points
+        points_file = self.write_analysis_grids(project_folder, project_name)
+
+        # 2.write sun files
+        ann = Analemma(self.sun_vectors, self.hoys)
+        ann.execute(project_folder + '/sky')
+        sun_modifiers = os.path.join(project_folder + '/sky', ann.sunlist_file)
+        suns_geo = os.path.join(project_folder + '/sky', ann.analemma_file)
+
+        # 2.1.add sun list to modifiers
+        self._radiance_parameters.mod_file = self.relpath(sun_modifiers, project_folder)
+        self._radiance_parameters.y_dimension = self.total_point_count
+
+        # 3.write batch file
+        if header:
+            self._commands.append(self.header(project_folder))
+
+        # TODO(Mostapha): add window_groups here if any!
+        # # 4.1.prepare oconv
+        oct_scene_files = opqfiles + glzfiles + wgsfiles + [suns_geo] + \
+            extrafiles.fp
+
+        oct_scene_files_items = []
+        for f in oct_scene_files:
+            if isinstance(f, (list, tuple)):
+                print('Point-in-time recipes cannot currently handle dynamic window'
+                      ' groups. The first state will be used for simulation.')
+                oct_scene_files_items.append(f[0])
+            else:
+                oct_scene_files_items.append(f)
+        oc = Oconv(project_name)
+        oc.scene_files = tuple(self.relpath(f, project_folder)
+                               for f in oct_scene_files_items)
+
+        # # 4.2.prepare Rcontrib
+        rct = Rcontrib('result/total..' + project_name,
+                       rcontrib_parameters=self._radiance_parameters)
+        rct.octree_file = str(oc.output_file)
+        rct.points_file = self.relpath(points_file, project_folder)
+
+        rmtx = rgb_matrix_file_to_ill((str(rct.output_file),),
+                                      'result/total..{}.ill'.format(project_name))
+        # # 4.3 write batch file
+        self._commands.append(oc.to_rad_string())
+        self._commands.append(rct.to_rad_string())
+        self._commands.append(rmtx.to_rad_string())
+
+        # add rcontrib for single reflection
+        self._radiance_parameters.direct_sec_relays = 0
+        rct2 = Rcontrib('result/sun..' + project_name,
+                        rcontrib_parameters=self._radiance_parameters)
+        rct2.octree_file = str(oc.output_file)
+        rct2.points_file = self.relpath(points_file, project_folder)
+
+        rmtx2 = rgb_matrix_file_to_ill((str(rct2.output_file),),
+                                       'result/sun..{}.ill'.format(project_name))
+
+        # add the last two lines to batch file.
+        self._commands.append(rct2.to_rad_string())
+        self._commands.append(rmtx2.to_rad_string())
+
+        self._result_files = os.path.join(project_folder, str(rmtx.output_file))
+
+        batch_file = os.path.join(project_folder, "commands.bat")
+        return write_to_file(batch_file, '\n'.join(self.commands))
+
+    def results(self):
+        """Return results for this analysis."""
+        assert self._isCalculated, \
+            "You haven't run the Recipe yet. Use self.run " + \
+            "to run the analysis before loading the results."
+
+        print('Unloading the current values from the analysis grids.')
+        for ag in self.analysis_grids:
+            ag.unload()
+
+        hours = tuple(int(self.timestep * h) for h in self.hoys)
+        rf = self._result_files
+        df = rf.replace('total..', 'sun..')
+        start_line = 0
+        for count, analysisGrid in enumerate(self.analysis_grids):
+            if count:
+                start_line += len(self.analysis_grids[count - 1])
+
+            # TODO(): Add timestep
+            analysisGrid.set_coupled_values_from_file(
+                rf, df, hours, start_line=start_line,
+                header=True, check_point_count=False, mode=1
+            )
+
+        return self.analysis_grids
+
+    def to_json(self):
+        """Create the solar access recipe from json.
+            {
+              "id": "solar_access",
+              "type": "gridbased",
+              "location": null, // a honeybee location - see below
+              "hoys": [], // list of hours of the year
+              "surfaces": [], // list of honeybee surfaces
+              "analysis_grids": [], // list of analysis grids
+              "sun_vectors": []
+            }
+        """
+        raise NotImplementedError()
+        return {
+            "id": "direct_reflection",
+            "type": "gridbased",
+            "location": None,
+            "hoys": self.hoys,
+            "surfaces": [srf.to_json() for srf in self.hb_objects],
+            "analysis_grids": [ag.to_json() for ag in self.analysis_grids],
+            "sun_vectors": [tuple(-1 * c for c in v) for v in self.sun_vectors]
+        }
