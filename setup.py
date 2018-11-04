f77009dd5f01474cf1510fe18f639d6d8dd86b68
LAPTOP-MB0HU4QI\Mostapha <sadeghipour@gmail.com>
2018-11-05 07:19:31 +1100

    feat(*): prepare to push to PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,40 @@
-from setuptools import setup, find_packages
+import re
+import setuptools
+import sys
 
-setup(
-    name='honeybee',
-    packages=find_packages()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('honeybee/__init__.py', 'r') as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(),
+        re.MULTILINE
+    ).group(1)
+
+try:
+    from semantic_release import setup_hook
+    setup_hook(sys.argv)
+except ImportError:
+    pass
+
+setuptools.setup(
+    name="lbt-honeybee",
+    version=version,
+    author="Ladybug Tools",
+    author_email="info@ladybug.tools",
+    description="Honeybee is a Python library to create, run and visualize the results of daylight (RADIANCE) and energy analysis (EnergyPlus/OpenStudio). The current version supports only Radiance integration.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladybug-tools/honeybee",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'lbt-ladybug'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ],
 )
